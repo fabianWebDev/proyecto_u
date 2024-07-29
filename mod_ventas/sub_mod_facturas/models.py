54a93a57c3921ed597053703b24841aaa45e2df9
fabianWebDev <fabiancava22@gmail.com>
2024-07-29 17:05:07 -0600

    Facturas module update - Factura model created

--- mod_ventas/sub_mod_facturas/models.py
+++ mod_ventas/sub_mod_facturas/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
+from datetime import date
+from mod_ventas.sub_mod_productos.models import Producto
 
-# Create your models here.
+class Factura(models.Model):
+    numero_factura = models.IntegerField(default=0)
+    fecha_emision = models.DateField(default=date.today)
+    total = models.FloatField(default=0)
+    # Define related fields if needed
+    # tipo_metodo_pago = models.ForeignKey(MetodoPago, on_delete=models.SET_DEFAULT, default=1)
+    # empleado = models.ForeignKey(Empleado, on_delete=models.SET_DEFAULT, default=1)
+    # cliente = models.ForeignKey(Cliente, on_delete=models.SET_DEFAULT, default=1)
+    # servicio_envio = models.ForeignKey(TipoServicio, on_delete=models.SET_DEFAULT, default=1)
+
+    def get_absolute_url(self):
+        return reverse("proveedor_detalle", args=[self.id])  # Adjust as needed
+
+class FacturaDetalle(models.Model):
+    factura = models.ForeignKey(Factura, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)  # Adjust as needed
+    cantidad = models.PositiveIntegerField(default=1)
+    precio_unitario = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f"{self.producto} - {self.cantidad} units"
