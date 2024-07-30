7e31674859449167fed10d78f9d322dcfd0c155e
fabianWebDev <fabiancava22@gmail.com>
2024-07-29 18:09:45 -0600

    Tipo producto model create - Producto model update

--- mod_ventas/sub_mod_productos/admin.py
+++ mod_ventas/sub_mod_productos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Producto, TipoProducto
 
 class ProductoAdmin(admin.ModelAdmin):
     list_filter = ('id', 'nombre',)
     list_display = ('id', 'nombre', 'descripcion', 'precio_venta', 'precio_compra', 'stock', 'codigo_lote')
 
 admin.site.register(Producto, ProductoAdmin)
+admin.site.register(TipoProducto)
